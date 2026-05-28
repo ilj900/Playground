d9454ee91f2748f0c5d7955db3f7b88df9e1e29c
ilj900 <ilj900@gmail.com>
2026-05-28 18:01:22 +0200

    Simple RNG noise

--- src/rng_test.cpp
+++ src/rng_test.cpp
@@ -20,7 +20,7 @@ TEST(Random, RandomImage)
     auto Image = ImageWrapperFactory::CreateRGB8(ImageWidth, ImageWidth);
 
     auto RNG = RNG::GetDeterministicUniformFloatRNG(0, 1);
-    for (uint32_t i = 0; i < 25 * ImageWidth * ImageWidth; i++)
+    for (uint32_t i = 0; i < 32 * ImageWidth * ImageWidth; i++)
     {
         float X = RNG();
         float Y = RNG();
@@ -29,5 +29,23 @@ TEST(Random, RandomImage)
         (*Image)[X_i, Y_i].R = static_cast<uint8_t>(std::min((*Image)[X_i, Y_i].R + 1u, 255u));
     }
 
-    Image->Write("test.png");
+    Image->Write("SimpleRandom.png");
+}
+
+TEST(Random, RandomDiscrepancy)
+{
+    uint32_t ImageWidth = 1024;
+    auto Image = ImageWrapperFactory::CreateRGB8(ImageWidth, ImageWidth);
+
+    auto RNG = RNG::GetDeterministicUniformFloatRNG(0, 1);
+    for (uint32_t i = 0; i < ImageWidth * 16; i++)
+    {
+        float X = RNG();
+        float Y = RNG();
+        uint32_t X_i = uint32_t(X * ImageWidth);
+        uint32_t Y_i = uint32_t(Y * ImageWidth);
+        (*Image)[X_i, Y_i] = {255, 255, 255};
+    }
+
+    Image->Write("RandomDiscrepancy.png");
 }
